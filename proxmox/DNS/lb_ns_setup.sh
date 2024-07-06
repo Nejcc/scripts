78287a69c529e96d3bdb5228ad231fcfc3fd710e
Nejc <nejc.cotic@gmail.com>
2024-07-06 11:22:31 +0200

    u

--- proxmox/DNS/lb_ns_setup.sh
+++ proxmox/DNS/lb_ns_setup.sh
@@ -1,22 +1,94 @@
 #!/bin/bash
 
-# Array of CTIDs for Load Balancers
-lb_ctids=(10000 10001)
-lb_hostnames=("lb01.local" "lb02.local")
-lb_ips=("192.168.1.10/24" "192.168.1.11/24")
-memory=2048
-swap=512
-cores=2
-disk=25
-storage="local-lvm"
+# Function to prompt for user input with default values
+prompt_with_default() {
+  local prompt=$1
+  local default=$2
+  read -p "$prompt [$default]: " input
+  echo "${input:-$default}"
+}
 
-for i in "${!lb_ctids[@]}"; do
-  echo "Configuring Load Balancer ${lb_hostnames[$i]} with CTID ${lb_ctids[$i]}"
+# Gather user input
+num_lb=$(prompt_with_default "How many load balancers (LB) do you have" "2")
+lb_ips=$(prompt_with_default "Provide IP list of load balancers (comma-separated)" "192.168.1.10,192.168.1.11")
+num_ns=$(prompt_with_default "How many name servers (NS) do you have" "4")
+ns_ips=$(prompt_with_default "Provide IP list of name servers (space-separated)" "192.168.1.20 192.168.1.21 192.168.1.22 192.168.1.23")
 
-  pct set ${lb_ctids[$i]} --hostname ${lb_hostnames[$i]}
-  pct set ${lb_ctids[$i]} --net0 name=eth0,bridge=vmbr0,ip=${lb_ips[$i]}
-  pct set ${lb_ctids[$i]} --memory $memory --swap $swap --cores $cores
-  pct set ${lb_ctids[$i]} --rootfs $storage:$disk
-  
-  echo "Load Balancer ${lb_hostnames[$i]} configured"
-done
+# Convert IP lists to arrays
+IFS=',' read -r -a lb_ip_array <<< "$lb_ips"
+IFS=' ' read -r -a ns_ip_array <<< "$ns_ips"
+
+# HAProxy configuration function
+configure_haproxy() {
+  local ctid=$1
+  local ns_ips=("${@:2}")
+  local backend_config="backend dns_backend\n    mode tcp\n    balance roundrobin\n"
+  for ns_ip in "${ns_ips[@]}"; do
+    backend_config+="    server ns${ns_ip##*.} $ns_ip:53 check\n"
+  done
+
+  local config="
+frontend dns_frontend
+    bind *:53
+    mode tcp
+    default_backend dns_backend
+
+$backend_config
+"
+  echo -e "$config" > /tmp/haproxy.cfg
+  pct push $ctid /tmp/haproxy.cfg /etc/haproxy/haproxy.cfg
+  pct exec $ctid -- systemctl restart haproxy
+}
+
+# Function to install and configure HAProxy on load balancers
+setup_load_balancers() {
+  for i in "${!lb_ip_array[@]}"; do
+    local ctid=10000+$i
+    echo "Configuring Load Balancer ${lb_ip_array[$i]} with CTID $ctid"
+
+    pct set $ctid --hostname "lb0$(($i + 1)).local"
+    pct set $ctid --net0 name=eth0,bridge=vmbr0,ip=${lb_ip_array[$i]}/24
+    pct set $ctid --memory 2048 --swap 512 --cores 2
+    pct set $ctid --rootfs local-lvm:25G
+    
+    # Install and configure HAProxy
+    pct exec $ctid -- apt-get update
+    pct exec $ctid -- apt-get install -y haproxy
+    configure_haproxy $ctid "${ns_ip_array[@]}"
+    
+    echo "Load Balancer ${lb_ip_array[$i]} configured with HAProxy"
+  done
+}
+
+# Function to install Pi-hole and GravitySync on name servers
+setup_name_servers() {
+  for i in "${!ns_ip_array[@]}"; do
+    local ctid=10010+$i
+    echo "Configuring Name Server ${ns_ip_array[$i]} with CTID $ctid"
+
+    pct set $ctid --hostname "ns0$(($i + 1)).local"
+    pct set $ctid --net0 name=eth0,bridge=vmbr0,ip=${ns_ip_array[$i]}/24
+    pct set $ctid --memory 2048 --swap 512 --cores 2
+    pct set $ctid --rootfs local-lvm:25G
+    
+    # Install Pi-hole and GravitySync
+    pct exec $ctid -- bash -c "$(curl -sSL https://install.pi-hole.net)"
+    pct exec $ctid -- bash -c "curl -sSL https://gravitysync.com/install.sh | bash"
+    
+    # Configure firewall to allow communication with load balancers
+    pct exec $ctid -- apt-get update
+    pct exec $ctid -- apt-get install -y ufw
+    pct exec $ctid -- ufw allow 22/tcp
+    pct exec $ctid -- ufw allow 53/tcp
+    for lb_ip in "${lb_ip_array[@]}"; do
+      pct exec $ctid -- ufw allow from $lb_ip
+    done
+    pct exec $ctid -- ufw enable
+    
+    echo "Name Server ${ns_ip_array[$i]} configured with Pi-hole, GravitySync, and firewall rules"
+  done
+}
+
+# Run setup functions
+setup_load_balancers
+setup_name_servers
